67ce2201fcacbc2b49c2684cba2429d497b2c4e9
jschoi <cjs0818@ust.ac.kr>
2023-08-01 19:30:51 +0900

    odas_server.py server proogram listening to odaslive has been implemented

---
+++ MicArray_conversation/odas_server.py
@@ -0,0 +1,90 @@
+import socket
+import json
+import threading
+
+def process_ssl_sst_result(result_str):
+    # Parse the JSON data to extract relevant information
+    try:
+        data_stream = json.loads(result_str)
+
+        if "src" in data_stream:
+            data = data_stream["src"]
+            #print(data)
+            #print(f"--- # of sources: {len(data)} ---")
+
+            # json example of 'ssl':
+            #{
+            #    "timeStamp": 285,
+            #    "src": [
+            #        { "x": -0.082, "y": -0.957, "z": 0.279, "E": 0.216 },
+            #        { "x": 0.196, "y": 0.064, "z": 0.979, "E": 0.131 },
+            #        { "x": 0.844, "y": 0.523, "z": 0.117, "E": 0.039 },
+            #        { "x": 0.196, "y": 0.847, "z": 0.495, "E": 0.009 }
+            #    ]
+            #}
+
+            # json example of 'sst':
+            #{
+            #    "timeStamp": 1071,
+            #    "src": [
+            #        { "id": 0, "tag": "", "x": 0.000, "y": 0.000, "z": 0.000, "activity": 0.000 },
+            #        { "id": 0, "tag": "", "x": 0.000, "y": 0.000, "z": 0.000, "activity": 0.000 },
+            #        { "id": 0, "tag": "", "x": 0.000, "y": 0.000, "z": 0.000, "activity": 0.000 },
+            #        { "id": 0, "tag": "", "x": 0.000, "y": 0.000, "z": 0.000, "activity": 0.000 }
+            #    ]
+            #}
+
+
+            if 'E' in data[0]: # ssl
+                print("      --- ssl ---")
+                for id in range(len(data)):
+                    azimuth = data[id]['x']
+                    elevation = data[id]['y']
+                    print(f"       ssl: Source detected at azimuth: {azimuth}, elevation: {elevation}")                
+            elif 'activity' in data[0]:    # sst
+                print("   #------ sst : start ------#")
+                for id in range(len(data)):
+                    if data[id]['activity'] > 0:
+                        print(f"   sst: {data[id]}")
+                print("   #------ sst : end ------#")
+            else:
+                print("No valid data found")
+
+    except:
+        print("json load error")
+
+
+def launch_socket_server(ip, port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((ip, port))
+            server_socket.listen()
+            print(f"Server listening on {ip}:{port}")
+
+            while True:
+                client_socket, client_addr = server_socket.accept()
+                print(f"Accepted connection from {client_addr}")
+
+                while True:
+                    data = client_socket.recv(4096).decode().strip()
+                    if not data:
+                        break
+                    #process_ssl_result(data) 
+                    process_ssl_sst_result(data)    
+
+    except KeyboardInterrupt:
+        print("Server shutdown.")
+
+
+if __name__ == "__main__":
+    # Replace "ODAS_SERVER_IP" and "ODAS_SERVER_PORT" with the desired IP and port for the server
+    odas_server_ip = "192.168.1.6"
+    odas_server_ssl_port = 9001
+    odas_server_sst_port = 9000
+
+    server_thread_ssl = threading.Thread(target=launch_socket_server, args=(odas_server_ip, odas_server_ssl_port))
+    server_thread_ssl.start()
+
+    server_thread_sst = threading.Thread(target=launch_socket_server, args=(odas_server_ip, odas_server_sst_port))
+    server_thread_sst.start()
+
